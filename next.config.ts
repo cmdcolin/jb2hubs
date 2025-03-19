c0fc9888a341f99dc8fd299bac7d29599d4cc0b2
Colin <colin.diesh@gmail.com>
2025-03-19 00:54:42 -0400

    Deploy static

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  output: 'export',
+  basePath: '/jb2hubs',
 }
 
 export default nextConfig
