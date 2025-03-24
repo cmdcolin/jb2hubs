f7bf5d9015adfba9b1115b8fffae589174237131
Colin <colin.diesh@gmail.com>
2025-03-24 14:53:52 -0400

    Download ncbi info (WIP)

--- ncbi.sh
+++ ncbi.sh
@@ -5,6 +5,6 @@ fd meta.json hubs | while read p; do
   dir=$(dirname $p)
   id=$(basename $dir)
   echo $id
-  (esearch -db assembly -query $id | esummary -mode json) >$dir/ncbi.json
+  (esearch -db assembly -query $id </dev/null | esummary -mode json) >$dir/ncbi.json
   sleep 0.1
 done
