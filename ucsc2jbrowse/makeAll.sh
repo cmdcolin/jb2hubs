bc28cc477b2c356177791f22c15f062700a71914
Colin <colin.diesh@gmail.com>
2025-04-29 12:52:41 -0700

    Restore aws sync

--- ucsc2jbrowse/makeAll.sh
+++ ucsc2jbrowse/makeAll.sh
@@ -15,6 +15,6 @@ export NODE_OPTIONS="--no-warnings=ExperimentalWarning"
 ./addMetadata.sh ~/ucscResults/*
 node src/makeUcscExtensions.ts ~/ucscResults
 ./getFileListing.sh ~/ucscResults/
-# aws s3 sync ~/ucscResults s3://jbrowse.org/ucsc/ --size-only
-# aws cloudfront create-invalidation --distribution-id E13LGELJOT4GQO --paths "/ucsc/*"
+aws s3 sync ~/ucscResults s3://jbrowse.org/ucsc/ --size-only
+aws cloudfront create-invalidation --distribution-id E13LGELJOT4GQO --paths "/ucsc/*"
 fd config.json ~/ucscResults/ | xargs -I {} bash -c 'cp "{}" configs/"$(basename "$(dirname "{}")").json"'
