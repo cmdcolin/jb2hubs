ce04c3f886cceb0fb4c09098f3b7699506545699
Colin <colin.diesh@gmail.com>
2025-04-30 11:25:47 -0700

    Force index upload

--- ucsc2jbrowse/makeAll.sh
+++ ucsc2jbrowse/makeAll.sh
@@ -16,7 +16,12 @@ export PATH=$(pwd):$PATH
 ./addMetadata.sh ~/ucscResults/*
 node src/makeUcscExtensions.ts ~/ucscResults
 ./getFileListing.sh ~/ucscResults/
+
+## size only on most...would be good to avoid this but it updates based on file times
 aws s3 sync --delete --size-only ~/ucscResults s3://jbrowse.org/ucsc/
+## include indexes without size-only
+aws s3 sync --delete --exclude="*" --include="*.csi" ~/ucscResults s3://jbrowse.org/ucsc/
+
 aws cloudfront create-invalidation --distribution-id E13LGELJOT4GQO --paths "/ucsc/*"
 fd config.json ~/ucscResults/ | xargs -I {} bash -c 'cp "{}" configs/"$(basename "$(dirname "{}")").json"'
 yarn prettier --write .
