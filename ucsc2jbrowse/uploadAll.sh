be8fc8c21387f015913b506f14d3e4f1fa013159
Colin <colin.diesh@gmail.com>
2025-05-10 15:37:22 -0700

    Upload script tweaks

--- ucsc2jbrowse/uploadAll.sh
+++ ucsc2jbrowse/uploadAll.sh
@@ -1,3 +1,9 @@
-aws s3 sync --delete --exclude="*meta.json" --exclude "*.hash" ~/ucscResults s3://jbrowse.org/ucsc/
+#!/bin/bash
+
+# use size-only for most everything, hope that things don't differ by 1 byte...
+aws s3 sync --delete --size-only --exclude="*meta.json" --exclude "*.hash" ~/ucscResults s3://jbrowse.org/ucsc/
+
+# don't use size-only for csi files, because they sometimes do only differ by 1 byte
+aws s3 sync --delete --exclude="*" --include "*.csi" ~/ucscResults s3://jbrowse.org/ucsc/
 
 aws cloudfront create-invalidation --distribution-id E13LGELJOT4GQO --paths "/ucsc/*"
