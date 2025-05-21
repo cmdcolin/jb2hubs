e93ebcc6983fe1a42acdd704f8a8d91071657b0d
Colin <colin.diesh@gmail.com>
2025-05-20 21:24:04 -0700

    Verbose rclone

--- ucsc2jbrowse/uploadAll.sh
+++ ucsc2jbrowse/uploadAll.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # We use rclone because it has the ability to checksum, compared with plain aws sync (which often will re-upload exact same file, with updated filetime)
-rclone sync -c --exclude "*.hash" --exclude "*_meta.json" ~/ucscResults jbrowse-data:jbrowse.org/ucsc
+rclone sync -c -v --exclude "*.hash" --exclude "*_meta.json" ~/ucscResults jbrowse-data:jbrowse.org/ucsc
 
 aws cloudfront create-invalidation --distribution-id E13LGELJOT4GQO --paths "/ucsc/*"
