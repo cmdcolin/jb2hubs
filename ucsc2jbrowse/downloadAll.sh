8f9c70af2648247c075e96b07f0c5e5b6de9265a
Colin <colin.diesh@gmail.com>
2025-05-21 17:23:17 -0700

    Consolidate

--- ucsc2jbrowse/downloadAll.sh
+++ ucsc2jbrowse/downloadAll.sh
@@ -5,4 +5,23 @@ export PATH=$(pwd):$PATH
 mkdir -p ~/ucscResults
 curl https://api.genome.ucsc.edu/list/ucscGenomes >~/ucscResults/list.json
 
-./downloadGoldenpath.sh ~/ucsc
+: ${OUT:=~/ucsc}
+: ${OUT2:=~/ucscAlt}
+
+curl https://api.genome.ucsc.edu/list/ucscGenomes | jq -r '.ucscGenomes | keys[]' | while
+  read p
+do
+  if [ "$p" = "cb1" ] || [ "$p" = "hs1" ]; then
+    echo "Skipping $p genome"
+    continue
+  fi
+  echo "rsync $OUT/$p"
+  mkdir -p $OUT/$p/$p
+  rsync --max-size=2G -qavzP rsync://hgdownload.cse.ucsc.edu/goldenPath/$p/database $OUT/$p/$p/
+done
+
+for p in hgFixed; do
+  echo "rsync $OUT2/$p"
+  mkdir -p $OUT2/$p/$p
+  rsync --max-size=2G -azP rsync://hgdownload.cse.ucsc.edu/goldenPath/hgFixed/database $OUT2/$p/$p/
+done
