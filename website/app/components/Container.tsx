6b1241dc675ab79410e659707e0c772332790a59
Colin <colin.diesh@gmail.com>
2025-06-08 19:14:02 -0400

    Add image

--- website/app/components/Container.tsx
+++ website/app/components/Container.tsx
@@ -1,3 +1,8 @@
 export default function Container({ children }: { children: React.ReactNode }) {
-  return <div className="lg:w-1/2 m-auto">{children}</div>
+  return (
+    <div className="lg:w-1/2 m-auto">
+      {children}
+      <div style={{ height: 300 }} />
+    </div>
+  )
 }
