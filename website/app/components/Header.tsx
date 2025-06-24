e5c25be9e9e7bb6037ece0570a81e7251492a445
Colin <colin.diesh@gmail.com>
2025-06-24 13:55:52 -0400

    Consider basePath empty

--- website/app/components/Header.tsx
+++ website/app/components/Header.tsx
@@ -1,37 +1,53 @@
 'use client'
 
-import { useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 
 import Link from 'next/link'
 
-export default function Header() {
+// method for tracking open state from https://stackoverflow.com/a/76884775/2129219
+function GenomesDropdown() {
   const [open, setOpen] = useState(false)
+  const detailsRef = useRef<HTMLDetailsElement>(null)
+  const onToggleCallback = useCallback(() => {
+    if (detailsRef.current) {
+      setOpen(detailsRef.current.open)
+    }
+  }, [])
 
+  useEffect(() => {
+    detailsRef.current?.addEventListener('toggle', onToggleCallback)
+
+    return () => {
+      detailsRef.current?.removeEventListener('toggle', onToggleCallback)
+    }
+  }, [onToggleCallback])
+  return (
+    <details className="dropdown" open={open} ref={detailsRef}>
+      <summary className="btn m-1">Genomes</summary>
+      <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+        <li>
+          <Link href="/ucsc" onClick={() => setOpen(false)}>
+            Main browsers
+          </Link>
+        </li>
+        <li>
+          <Link href="/genark" onClick={() => setOpen(false)}>
+            GenArk browsers
+          </Link>
+        </li>
+      </ul>
+    </details>
+  )
+}
+
+export default function Header() {
   return (
     <div className="navbar">
       <div className="navbar-start">
         <Link href="/" className="btn btn-ghost text-xl">
           JBrowse 2 hubs
         </Link>
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <details open={open} onClick={() => setOpen(!open)}>
-              <summary>Genomes</summary>
-              <ul className="p-2">
-                <li>
-                  <Link href="/ucsc" onClick={() => setOpen(false)}>
-                    Main browsers
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/genark" onClick={() => setOpen(false)}>
-                    GenArk browsers
-                  </Link>
-                </li>
-              </ul>
-            </details>
-          </li>
-        </ul>
+        <GenomesDropdown />
       </div>
     </div>
   )
