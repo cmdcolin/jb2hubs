6f5a4797eb7de7003c8f878622748ebb7986344d
Colin <colin.diesh@gmail.com>
2025-07-17 14:18:48 -0400

    Misc1

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -1,10 +1,13 @@
 import { NuqsAdapter } from 'nuqs/adapters/next/app'
+import { createJBrowseTheme } from '@jbrowse/core/ui/theme'
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter'
 
 import Header from './components/Header.tsx'
 
 import type { Metadata } from 'next'
 
 import './globals.css'
+import { ThemeProvider } from '@mui/material'
 
 export const metadata: Metadata = {
   title: 'JBrowse 2 hubs',
@@ -27,10 +30,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <NuqsAdapter>
-          <Header />
-          {children}
-        </NuqsAdapter>
+        <AppRouterCacheProvider>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline
+            to build upon. */}
+          <NuqsAdapter>
+            <Header />
+            {children}
+          </NuqsAdapter>
+        </AppRouterCacheProvider>
       </body>
     </html>
   )
