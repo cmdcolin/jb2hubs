cbf06ed700fd5c57fee3547a97ee9234039c0fea
Colin <colin.diesh@gmail.com>
2025-08-07 12:23:09 -0400

    Favicon

--- website/src/components/OrangeStar.jsx
+++ website/src/components/OrangeStar.jsx
@@ -3,5 +3,12 @@ import { Star } from 'lucide-react'
 import styles from './OrangeStar.module.css'
 
 export default function OrangeStar() {
-  return <Star fill="orange" strokeWidth={0} className={styles.orangeStar} />
+  return (
+    <Star
+      fill="orange"
+      strokeWidth={0}
+      className={styles.orangeStar}
+      title="designated reference"
+    />
+  )
 }
--- website/src/components/RedX.jsx
+++ website/src/components/RedX.jsx
@@ -3,5 +3,5 @@ import { X } from 'lucide-react'
 import styles from './RedX.module.css'
 
 export default function RedX() {
-  return <X stroke="red" className={styles.redX} />
+  return <X stroke="red" className={styles.redX} title="RefSeq suppressed" />
 }
