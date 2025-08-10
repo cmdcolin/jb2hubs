9df4efadb991d10e9ad37c9a7db83a1e2827019d
Colin <colin.diesh@gmail.com>
2025-08-10 09:51:26 -0400

    Pure css tooltip

--- website/src/components/OrangeStar.tsx
+++ website/src/components/OrangeStar.tsx
@@ -1,7 +1,15 @@
 import { Star } from 'lucide-react'
 
 import styles from './OrangeStar.module.css'
+import tooltipStyles from './Tooltip.module.css'
 
 export default function OrangeStar() {
-  return <Star fill="orange" strokeWidth={0} className={styles.orangeStar} />
+  return (
+    <span
+      className={tooltipStyles.tooltip}
+      data-tooltip="NCBI designated reference"
+    >
+      <Star fill="orange" strokeWidth={0} className={styles.orangeStar} />
+    </span>
+  )
 }
--- website/src/components/RedX.tsx
+++ website/src/components/RedX.tsx
@@ -1,10 +1,14 @@
 import { X } from 'lucide-react'
 
 import styles from './RedX.module.css'
+import tooltipStyles from './Tooltip.module.css'
 
 export default function RedX() {
   return (
-    <span className={styles.tooltip} data-tooltip="RefSeq suppressed">
+    <span
+      className={tooltipStyles.tooltip}
+      data-tooltip="NCBI RefSeq suppressed"
+    >
       <X stroke="red" className={styles.redX} />
     </span>
   )
