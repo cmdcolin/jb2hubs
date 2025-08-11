eaa037fd888ecb7181b07eb417c47e55cf9a3cea
Colin <colin.diesh@gmail.com>
2025-08-11 04:59:16 -0700

    Update filelisting to be just gff

--- genark2jbrowse/getFileListing.sh
+++ genark2jbrowse/getFileListing.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find ../hubs/ -type f ! -name "*meta.json" ! -name "*.xxh" ! -name "*.hash" ! -name "*.ix" ! -name "*.ixx" ! -name "*.notfound" ! -name "*image.json" ! -name "*hub.txt" | parallel --bar ./hash_if_needed.sh {} | sort -k2,2 >fileListing.txt
+find ../hubs/ -type f -name "*.gff.gz" | parallel --bar ./hash_if_needed.sh {} | sort -k2,2 >fileListing.txt
