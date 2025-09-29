3dfca40d2e222116b6b5251cacf43f1c90cd5a57
Colin <colin.diesh@gmail.com>
2025-09-29 09:54:24 -0700

    Hash config not working

--- ucsc2jbrowse/uploadAll.sh
+++ ucsc2jbrowse/uploadAll.sh
@@ -16,7 +16,7 @@ set -euo pipefail
 
 echo "Syncing JBrowse data to S3..."
 # We use rclone because it has the ability to checksum, compared with plain aws sync (which often will re-upload exact same file, with updated filetime)
-rclone sync -c -v --exclude "*.hash" --exclude "*.xxh" --exclude "*_meta.json" "$UCSC_RESULTS_DIR" jbrowse-data-hashed:jbrowse.org/ucsc --s3-storage-class INTELLIGENT_TIERING --checkers 20
+rclone sync -c -v --exclude "*.hash" --exclude "*.xxh" --exclude "*_meta.json" "$UCSC_RESULTS_DIR" jbrowse-data:jbrowse.org/ucsc --s3-storage-class INTELLIGENT_TIERING --checkers 20
 
 echo "Invalidating CloudFront cache..."
 aws cloudfront create-invalidation --distribution-id E13LGELJOT4GQO --paths "/ucsc/*"
